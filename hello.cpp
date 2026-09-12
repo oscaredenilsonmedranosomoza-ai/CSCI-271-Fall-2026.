0046ec634851fc9495da9e5ff4cbfa2c28dfba96
oscaredenilsonmedranosomoza-ai <oscaredenilsonmedranosomoza@gmail.com>
2026-09-11 21:11:45 -0400

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,21 @@
+// Oscar Medrano
+// Assignment 1
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello, world!" << endl;
+
+    string name;
+    cout << "What is your name? ";
+    cin >> name;
+
+    cout << "Nice to meet you, " << name << "!";
+
+    return 0;
+}
+ 
